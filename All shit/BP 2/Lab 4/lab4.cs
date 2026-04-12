11917fbe007198fcb4f42657742afdec258e18ca
Пащенко Артур ІM-52 <arthurpashchenko2007@gmail.com>
2026-04-12 21:14:40 +0300

    Надію виправдено ?

--- All shit/BP 2/Lab 4/lab4.cs
+++ All shit/BP 2/Lab 4/lab4.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Lab4;
@@ -15,18 +16,34 @@ public class BiDirectionalPriorityQueue<T>
 public readonly List<QueuePriority<T>> _items = new();
 private long _counter = 0L;
 
-public void Enqueu(T value, int priority)
+public void Enqueu(T value, int priority) // FIFO
 {
     _items.Add (new QueuePriority<T>
     {
     Value = value,
     Priority = priority,
     Id = (int) _counter++ // (int) - _counter — це long, але я його преобразував у int для зручності
+  });
+}
+
+public T Dequeue(Query type)
+    {
+        if (_items.Count == 0) throw new InvalidOperationException("Queue is empty");
+
+        var target = GetTarget(type);
+        _items.Remove(target);
+        return target.Value;
+    }
+
+    public T Peek(Query type)
+    {
+        if (_items.Count == 0) throw new InvalidOperationException("Queue is empty");
+
+        return GetTarget(type).Value;
+    }
+}
 
 
-});
-}
-}
 
 
 
