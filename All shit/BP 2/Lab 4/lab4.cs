e1adb4804c803cf5ac41cda226418e170eff4435
Пащенко Артур ІM-52 <arthurpashchenko2007@gmail.com>
2026-04-12 19:30:30 +0300

    Надіюсь правильно

---
+++ All shit/BP 2/Lab 4/lab4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab4;
+
+public enum Query {Highest, Lowest, Oldest, Newest}
+public class QueuePriority<T>
+{
+    public T Value {get; init;}
+    public int Priority {get; init;}
+    public int Id {get; init;}
+}
+public class BiDirectionalPriorityQueue<T>
+{
+public readonly List<QueuePriority<T>> _items = new();
+private long _counter = 0L;
+
+public void Enqueu(T value, int priority)
+{
+    _items.Add (new QueuePriority<T>
+    {
+    Value = value,
+    Priority = priority,
+    Id = (int) _counter++ // (int) - _counter — це long, але я його преобразував у int для зручності
+
+
+});
+}
+}
+
+
+
+
+
+
+
+
